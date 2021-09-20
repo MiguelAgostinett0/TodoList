8096e4324a97b486c7a4f61f23bd3e8d3649c809
jonatasFreireDevi <83781371+jonatasFreireDevi@users.noreply.github.com>
2021-09-20 12:24:10 -0300

    first commit

---
+++ src/components/InputTask.vue
@@ -0,0 +1,16 @@
+<tamplate>
+  <div>
+    <input class="new-todo"
+      placeholder="O que procisa ser feito?">
+  </div>
+</template>
+
+<script>
+inport { Task } from '../models/Task'
+
+export default {
+  data () {
+    return {
+    }
+  }
+}
